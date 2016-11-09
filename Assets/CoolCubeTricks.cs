14b09d8fd4debd07f9c4e8993ff2c7c0f1abe2fb
sa <sixsecondfiesta@gmail.com>
2016-11-08 21:27:55 -0500

    Cool cube tricks and scene

---
+++ Assets/CoolCubeTricks.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System;
+using System.Collections.Generic;
+using Debug = UnityEngine.Debug;
+using Object = UnityEngine.Object;
+using Random = UnityEngine.Random;
+
+public class CoolCubeTricks : MonoBehaviour
+{
+    void Start()
+    {
+        Debug.Log("I am a ice cold cube");
+    }
+    
+    void Update()
+    {
+        
+    }
+}
