50b2412dbd61308c9736e5de48c556b06bd38a75
sa <sixsecondfiesta@gmail.com>
2016-11-08 21:38:45 -0500

    cool cube trix pt 2.

--- Assets/CoolCubeTricks.cs
+++ Assets/CoolCubeTricks.cs
@@ -7,13 +7,17 @@ using Random = UnityEngine.Random;
 
 public class CoolCubeTricks : MonoBehaviour
 {
-    void Start()
-    {
-        Debug.Log("I am a ice cold cube");
-    }
-    
-    void Update()
-    {
-        
-    }
+  Renderer ren;
+
+  void Start()
+  {
+    Debug.Log("I am a ice cold cube");
+
+    ren = GetComponent<Renderer>();
+  }
+  
+  void Update()
+  {
+    ren.material.color = new Color(Random.value, Random.value, Random.value);
+  }
 }
